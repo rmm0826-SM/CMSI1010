aec844a019db0f3897e17200a5921b969bd1ec15
rmm0826-SM <rmm0826@icloud.com>
2026-09-21 17:12:17 -0700

    Be more lenient with the Petting Zoo input

--- lab04/zoo.py
+++ lab04/zoo.py
@@ -13,11 +13,12 @@ def show_all_animals():
 
 
 def pet_animal(animal):
-    if animal == "Clover":
+    print(animal)
+    if animal == "clover":
         print("Clover is so happy! ❤️")
-    elif animal == "Coco":
+    elif animal == "coco":
         print("Coco the Baby Goat thanks you! 🥰")
-    elif animal == "Arno":
+    elif animal == "arno":
         print("Actually, we cannot allow you to pet Arno. ⛔️")
     else:
         print("Sorry, I don't know that animal")
@@ -28,17 +29,17 @@ print("Type 'help' to get a list of all the things you can do")
 print()
 
 keep_going = True
-while keep_going: 
+while True:
     response = input("What would you like to do? ").strip().lower()
     if response == "help":
         show_help()
     elif response == "see":
         show_all_animals()
     elif response.startswith("pet "):
-        animal = response[4:]
+        animal = response[4:].strip()
         pet_animal(animal)
     elif response == "bye":
         print("Goodbye!")
-        keep_going = False
+        break
     else:
         print("Sorry, I don't understand that command.")
